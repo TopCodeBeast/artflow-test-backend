1d728e58e5a4a15bc0c975f0b559a0f01870228f
arnoldwang <arnoldprodev@gmail.com>
2022-12-05 14:56:33 +0900

    remove clustering

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-const cluster = require("cluster");
-const totalCPUs = require("os").cpus().length;
+// const cluster = require("cluster");
+// const totalCPUs = require("os").cpus().length;
 
-if (cluster.isMaster) {
-  console.log(`Number of CPUs is ${totalCPUs}`);
-  console.log(`Master ${process.pid} is running`);
+// if (cluster.isMaster) {
+//   console.log(`Number of CPUs is ${totalCPUs}`);
+//   console.log(`Master ${process.pid} is running`);
 
-  // Fork workers.
-  for (let i = 0; i < totalCPUs; i++) {
-    cluster.fork();
-  }
+//   // Fork workers.
+//   for (let i = 0; i < totalCPUs; i++) {
+//     cluster.fork();
+//   }
 
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-    console.log("Let's fork another worker!");
-    cluster.fork();
-  });
-} else {
-  const app = require("./app");
-  const appConfig = require("./config/app.config");
+//   cluster.on("exit", (worker, code, signal) => {
+//     console.log(`worker ${worker.process.pid} died`);
+//     console.log("Let's fork another worker!");
+//     cluster.fork();
+//   });
+// } else {
 
-  app.listen(appConfig.port, () => {
-    console.log(`Server is running on port ${appConfig.port}.`);
-  });
-}
+// }
+
+const app = require("./app");
+const appConfig = require("./config/app.config");
+
+app.listen(appConfig.port, () => {
+  console.log(`Server is running on port ${appConfig.port}.`);
+});
